aaf4ef29dd56c7d9f0eefc0687ff5c1e17f61427
Kent C. Dodds <kent+github@doddsfamily.us>
2016-07-26 11:40:29 -0600

    feat(init): add init script
    * chore(build): Ignore fixtures
    
    * feat(init): add init script
    
    This makes it easier for people to adopt p-s by simply running `node_modules/.bin/p-s init`
    
    Closes #35

---
+++ src/bin-utils/initialize/index.test.js
@@ -0,0 +1,60 @@
+import {resolve} from 'path'
+import {readFileSync} from 'fs'
+import test from 'ava'
+import proxyquire from 'proxyquire'
+import {spy} from 'sinon'
+
+test('normal case initialize', t => {
+  const packageScriptsDestination = resolve('./fixtures/package-scripts.js')
+  const packageJsonDestination = resolve('./fixtures/_package.json')
+  const expectedPackageScripts = readFileSync(resolve('./fixtures/_package-scripts.js'), 'utf-8')
+  const writeFileSync = spy()
+  const findUpSync = spy(file => {
+    if (file === 'package.json') {
+      return packageJsonDestination
+    }
+    throw new Error('Should not look for anything but package.json')
+  })
+  const initialize = proxyquire('./index', {
+    'find-up': {sync: findUpSync},
+    fs: {writeFileSync},
+  }).default
+
+  initialize()
+
+  const [packageScriptsDestinationResult, packageScriptsStringResult] = writeFileSync.firstCall.args
+  const [packageJsonDestinationReuslt, packageJsonStringResult] = writeFileSync.secondCall.args
+  const {scripts: packageJsonScripts} = JSON.parse(packageJsonStringResult)
+
+  t.true(writeFileSync.calledTwice)
+  t.is(packageScriptsDestinationResult, packageScriptsDestination)
+  t.is(packageScriptsStringResult, expectedPackageScripts)
+  t.is(packageJsonDestinationReuslt, packageJsonDestination)
+  t.deepEqual(packageJsonScripts, {
+    start: 'package-scripts',
+    test: 'package-scripts test',
+  })
+})
+
+test('initialize without a test script should not add a test to the package.json', t => {
+  const packageJsonDestination = resolve('./fixtures/_package-no-test.json')
+  const writeFileSync = spy()
+  const findUpSync = spy(file => {
+    if (file === 'package.json') {
+      return packageJsonDestination
+    }
+    throw new Error('Should not look for anything but package.json')
+  })
+  const initialize = proxyquire('./index', {
+    'find-up': {sync: findUpSync},
+    fs: {writeFileSync},
+  }).default
+
+  initialize()
+  const [, packageJsonStringResult] = writeFileSync.secondCall.args
+  const {scripts: packageJsonScripts} = JSON.parse(packageJsonStringResult)
+
+  t.deepEqual(packageJsonScripts, {
+    start: 'package-scripts',
+  })
+})
--- src/bin/p-s.js
+++ src/bin/p-s.js
@@ -3,12 +3,14 @@
 import findUp from 'find-up'
 import {merge} from 'lodash'
 import program from 'commander'
+import colors from 'colors/safe'
 import runPackageScript from '../index'
-import {getScriptsAndArgs, help, preloadModule, loadConfig} from '../bin-utils'
+import {getScriptsAndArgs, initialize, help, preloadModule, loadConfig} from '../bin-utils'
 import getLogger from '../get-logger'
 
 const log = getLogger()
 const FAIL_CODE = 1
+let initializing = false
 
 program
   .version(require('../../package.json').version)
@@ -18,17 +20,28 @@ program
   .option('-c, --config <filepath>', 'Config file to use (defaults to nearest package-scripts.js)')
   .option('-l, --log-level <level>', 'The log level to use (error, warn, info [default])')
   .option('-r, --require <module>', 'Module to preload')
+  .on('init', () => {
+    initializing = true
+    const {packageScriptsPath} = initialize()
+    log.info(`Your scripts have been saved at ${colors.green(packageScriptsPath)}`)
+    log.info(colors.gray(
+      'Check out your scripts in there. Go ahead and update them and add descriptions to the ones that need it'
+    ))
+    log.info(colors.gray('Your package.json scripts have also been updated. Run `npm start --help` for help'))
+  })
   .on('--help', onHelp)
   .parse(process.argv)
 
-const scriptsAndArgs = getScriptsAndArgs(program)
-if (scriptsAndArgs.scripts.length < 1) {
-  program.outputHelp()
-} else {
-  loadAndRun()
+if (!initializing) {
+  const scriptsAndArgs = getScriptsAndArgs(program)
+  if (scriptsAndArgs.scripts.length < 1) {
+    program.outputHelp()
+  } else {
+    loadAndRun(scriptsAndArgs)
+  }
 }
 
-function loadAndRun() {
+function loadAndRun(scriptsAndArgs) {
   const psConfig = getPSConfig()
 
   runPackageScript({
