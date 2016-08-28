d61c822ab4b0682f219ca19b9851b14e89102d1c
Kent C. Dodds <kent+github@doddsfamily.us>
2016-08-27 21:20:39 -0600

    feat(autocomplete): add `completion` script to install autocomplete (#45)
    * feat(autocomplete): add `completion` script to install autocomplete
    
    **What**: This adds a command called `completion` which installs the
    autocomplete shell script. It also adds the logic to provide the
    autocompletion.
    
    **Why**: Because that's pretty swell!
    
    **How**: Using omelette which is pretty awesome.
    
    * fix(autocomplete): omelette uses process.exit.... :-(
    
    * chore(message): Improve message when completion is installed
    
    * docs(autocomplete): improve accuracy of the docs
    
    * chore(scripts): Improve build script

--- src/bin/p-s.js
+++ src/bin/p-s.js
@@ -1,19 +1,25 @@
 #!/usr/bin/env node
 /* eslint no-process-exit: "off" */
 import findUp from 'find-up'
-import {merge} from 'lodash'
+import {merge, includes} from 'lodash'
 import program from 'commander'
 import colors from 'colors/safe'
 import runPackageScript from '../index'
-import {getScriptsAndArgs, initialize, help, preloadModule, loadConfig} from '../bin-utils'
+import {
+  getScriptsAndArgs, initialize, autocomplete, installAutocomplete,
+  help, preloadModule, loadConfig,
+} from '../bin-utils'
 import getLogger from '../get-logger'
+
+const {version} = require('../../package.json')
 
 const log = getLogger()
 const FAIL_CODE = 1
 let shouldRun = true
+const shouldAutocomplete = includes(process.argv, '--compbash')
 
 program
-  .version(require('../../package.json').version)
+  .version(version)
   .allowUnknownOption()
   .option('-s, --silent', 'Silent p-s output')
   .option('-p, --parallel <script-name1,script-name2>', 'Scripts to run in parallel (comma seprated)')
@@ -21,10 +27,14 @@ program
   .option('-l, --log-level <level>', 'The log level to use (error, warn, info [default])')
   .option('-r, --require <module>', 'Module to preload')
   .on('init', onInit)
+  .on('completion', onRequestToInstallCompletion)
   .on('--help', onHelp)
   .parse(process.argv)
 
-if (shouldRun) {
+
+if (shouldAutocomplete) {
+  autocomplete(getPSConfig())
+} else if (shouldRun) {
   const psConfig = getPSConfig()
   const hasDefaultScript = !!psConfig.scripts.default
   const scriptsAndArgs = getScriptsAndArgs(program)
@@ -73,9 +83,30 @@ function onInit() {
     'Check out your scripts in there. Go ahead and update them and add descriptions to the ones that need it'
   ))
   log.info(colors.gray('Your package.json scripts have also been updated. Run `npm start -- --help` for help'))
+  log.info(colors.gray(
+    'You may also want to install the package globally and installing autocomplete script. You can do so by running\n' +
+    '  npm install --global p-s\n' +
+    '  nps completion <optionally-your-bash-profile-file>\n' +
+    'The bash profile file defaults to ~/.bash_profile for bash and ~/.zshrc for zsh'
+  ))
 }
 
 function onHelp() {
   shouldRun = false
   log.info(help(getPSConfig()))
+}
+
+function onRequestToInstallCompletion() {
+  shouldRun = false
+  const [,,, destination] = process.argv
+  if (destination) {
+    log.info(`Installing p-s autocomplete into ${destination}`)
+  } else {
+    log.info('Installing p-s autocomplete into the default for your current terminal')
+  }
+  log.info(
+    `You're going to need to either resource that file, or open a new instance of ` +
+    'the terminal to get autocomplete to start working'
+  )
+  installAutocomplete(destination)
 }
