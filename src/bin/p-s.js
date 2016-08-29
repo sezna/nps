sezna/nps

commit a70e9a87fc8756d5ea42bb8fe5bd8a430866832c

feat(help): add help command (#46)
Update some docs as well.

src/bin/p-s.js
@@ -38,7 +38,10 @@ if (shouldAutocomplete) {
   const psConfig = getPSConfig()
   const hasDefaultScript = !!psConfig.scripts.default
   const scriptsAndArgs = getScriptsAndArgs(program)
+  const hasHelpScript = !!psConfig.scripts.help
   if (!hasDefaultScript && scriptsAndArgs.scripts.length < 1) {
+    program.outputHelp()
+  } else if (!hasHelpScript) { // eslint-disable-line no-negated-condition
     program.outputHelp()
   } else {
     loadAndRun(scriptsAndArgs, psConfig)
@@ -82,7 +85,7 @@ function onInit() {
   log.info(colors.gray(
     'Check out your scripts in there. Go ahead and update them and add descriptions to the ones that need it'
   ))
-  log.info(colors.gray('Your package.json scripts have also been updated. Run `npm start -- --help` for help'))
+  log.info(colors.gray('Your package.json scripts have also been updated. Run `npm start help` for help'))
   log.info(colors.gray(
     'You may also want to install the package globally and installing autocomplete script. You can do so by running\n' +
     '  npm install --global p-s\n' +

commit c487ea08e5417a070da11284c5153ab0fe8d11c5

fix(bin): only show help when it should
As noted by @boneskull. Commit a70e9a8 caused p-s to always output the
`help` unless you had a truthy help script configured

Thanks for the report @boneskull

src/bin/p-s.js
@@ -39,9 +39,11 @@ if (shouldAutocomplete) {
   const hasDefaultScript = !!psConfig.scripts.default
   const scriptsAndArgs = getScriptsAndArgs(program)
   const hasHelpScript = !!psConfig.scripts.help
-  if (!hasDefaultScript && scriptsAndArgs.scripts.length < 1) {
+  const scriptIsHelp = scriptsAndArgs.scripts[0] === 'help'
+  const scriptSpecified = scriptsAndArgs.scripts.length >= 1
+  if (!hasDefaultScript && !scriptSpecified) {
     program.outputHelp()
-  } else if (!hasHelpScript) { // eslint-disable-line no-negated-condition
+  } else if (!hasHelpScript && scriptIsHelp) { // eslint-disable-line no-negated-condition
     program.outputHelp()
   } else {
     loadAndRun(scriptsAndArgs, psConfig)