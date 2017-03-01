b789cc9280ddbc25cdaaebf7cb169b7fb49f97e4
Christopher Sparger <sloansparger@gmail.com>
2017-02-28 21:14:12 -0600

    WIP want to make sure this is what we actually want

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import spawn from 'spawn-command-with-kill'
 import chalk from 'chalk'
 import {oneLine} from 'common-tags'
-import {isString, clone} from 'lodash'
-import {sync as findUpSync} from 'find-up'
-import managePath from 'manage-path'
+import {isString} from 'lodash'
 import arrify from 'arrify'
 import getScriptToRun from './get-script-to-run'
 import getScriptsFromConfig from './get-scripts-from-config'
 import getLogger, {getLogLevel} from './get-logger'
+import getEnv from './get-env-variables'
 
 const NON_ERROR = 0
 
@@ -46,47 +45,43 @@ function runPackageScript({scriptConfig, options, input}) {
   const log = getLogger(getLogLevel(options))
   log.info(chalk.gray('nps executing: ') + chalk.green(command))
   let child
+
   return new Promise((resolve, reject) => {
-    child = spawn(command, {stdio: 'inherit', env: getEnv()})
+    getEnv()
+      .then(env => {
+        child = spawn(command, {stdio: 'inherit', env})
 
-    child.on('error', error => {
-      reject({
-        message: chalk.red(
-          oneLine`
-            The script called "${scriptPrefix}"
-            which runs "${command}" emitted an error
-          `,
-        ),
-        ref: 'emitted-an-error',
-        error,
-      })
-    })
-
-    child.on('close', code => {
-      if (code === NON_ERROR) {
-        resolve(code)
-      } else {
-        reject({
-          message: chalk.red(
-            oneLine`
-              The script called "${scriptPrefix}"
-              which runs "${command}" failed with exit code ${code}
-            `,
-          ),
-          ref: 'failed-with-exit-code',
-          code,
+        child.on('error', error => {
+          reject({
+            message: chalk.red(
+              oneLine`
+                The script called "${scriptPrefix}"
+                which runs "${command}" emitted an error
+              `,
+            ),
+            ref: 'emitted-an-error',
+            error,
+          })
         })
-      }
-    })
+
+        child.on('close', code => {
+          if (code === NON_ERROR) {
+            resolve(code)
+          } else {
+            reject({
+              message: chalk.red(
+                oneLine`
+                  The script called "${scriptPrefix}"
+                  which runs "${command}" failed with exit code ${code}
+                `,
+              ),
+              ref: 'failed-with-exit-code',
+              code,
+            })
+          }
+        })
+      })
+      .catch(err => console.log('PROMISE ERROR', err))
   })
 }
 
-function getEnv() {
-  const env = clone(process.env)
-  const alterPath = managePath(env)
-  const npmBin = findUpSync('node_modules/.bin')
-  if (npmBin) {
-    alterPath.unshift(npmBin)
-  }
-  return env
-}
